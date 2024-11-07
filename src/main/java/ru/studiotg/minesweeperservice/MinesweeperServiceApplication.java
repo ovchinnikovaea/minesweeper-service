e7b21137cdd269096ed2d1ae31fd211f5b7060b9
ovchinnikova <ov4innikova.e.a@mail.ru>
2024-11-07 21:42:03 +0300

    Added main file

---
+++ src/main/java/ru/studiotg/minesweeperservice/MinesweeperServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.studiotg.minesweeperservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesweeperServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinesweeperServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/studiotg/minesweeperservice/MinesweeperServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.studiotg.minesweeperservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweeperServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
